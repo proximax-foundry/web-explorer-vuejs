1660db8d07c15f6bec223ecf14ef2f4ac47d2ec2
Benjamin - workstation <benjamin@webtree.my>
2022-02-21 12:48:40 +0800

    Added sliding effect

--- src/modules/transaction/views/ViewTransaction.vue
+++ src/modules/transaction/views/ViewTransaction.vue
@@ -5,11 +5,13 @@
     </p>
     <div class="filter shadow-xl border border-gray-50 p-5 mb-15">
       <div class="flex items-center mb-4 border-b border-gray-100 relative">
-        <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'detail')?'cursor-pointer':'' }`" @click="currentPage = 'detail'">Overview<div v-if="currentPage == 'detail'" class="absolute w-full border-b border-yellow-500" style="bottom: -1px;"></div></div>
-        <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'inner')?'cursor-pointer':'' }`" @click="currentPage = 'inner'">Inner Txns<div v-if="currentPage == 'inner'" class="absolute w-full border-b border-yellow-500" style="bottom: -1px;"></div></div>
+        <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'detail')?'cursor-pointer':'' }`" @click="currentPage = 'detail'">Overview<div v-if="currentPage == 'detail'" class="absolute w-full border-b border-yellow-500 transition-all duration-200" style="bottom: -1px;"></div></div>
+        <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'inner')?'cursor-pointer':'' }`" @click="currentPage = 'inner'">Inner Txns<div v-if="currentPage == 'inner'" class="absolute w-full border-b border-yellow-500 transition-all duration-200" style="bottom: -1px;"></div></div>
       </div>
-      <TxnDetailComponent v-if="currentPage == 'detail'" />
-      <InnerTxnComponent v-else />
+      <transition name="slide">
+        <TxnDetailComponent v-if="currentPage == 'detail'" />
+        <InnerTxnComponent v-else />
+      </transition>
     </div>
   </div>
 </template>
@@ -38,4 +40,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.slide-enter-active {
+  -moz-transition-duration: 1s;
+  -webkit-transition-duration: 1s;
+  -o-transition-duration: 1s;
+  transition-duration: 1s;
+  -moz-transition-timing-function: ease-in-out;
+  -webkit-transition-timing-function: ease-in-out;
+  -o-transition-timing-function: ease-in-out;
+  transition-timing-function: ease-in-out;
+}
+
+.slide-leave-active {
+  -moz-transition-duration: 1s;
+  -webkit-transition-duration: 1s;
+  -o-transition-duration: 1s;
+  transition-duration: 1s;
+  -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  max-height: 1000px;
+  overflow: hidden;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  overflow: hidden;
+  max-height: 0;
+}
 </style>
