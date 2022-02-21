3393bd039c773a20a4372213f2397445170769d4
Benjamin - workstation <benjamin@webtree.my>
2022-02-21 12:01:30 +0800

    Fixed footer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import "./assets/scss/main.scss";
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import 'animate.css';
 import vueDebounce from 'vue-debounce'
-import { VuePassword } from 'vue-password';
 import mitt from 'mitt';
 import PrimeVue from 'primevue/config';
 import "primeicons/primeicons.css";
@@ -14,10 +14,8 @@ import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 
 // Import Font Awesome Icons
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faFacebookF, faTelegramPlane, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
-library.add(faFacebookF, faTelegramPlane, faTwitter, faGithub)
+
+
 
 import ConfirmDialog from 'primevue/confirmdialog';
 import Toast from 'primevue/toast';
@@ -38,10 +36,11 @@ app.use(vueDebounce);
 app.use(VueBlocksTree,defaultoptions)
 app.mount('#app');
 // Use Components
+
 app.component('ConfirmDialog', ConfirmDialog);
 app.component('Toast', Toast);
-app.component('font-awesome-icon', FontAwesomeIcon);
-app.component(VuePassword);
+
+
 
 
 
