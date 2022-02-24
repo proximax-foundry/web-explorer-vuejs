aad6aedb77401325d2d5a5d8a7cad990a5bd9a26
Benjamin - workstation <benjamin@webtree.my>
2022-02-24 15:48:27 +0800

    Fetch txn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="flex flex-col justify-between min-h-screen bg-white">
+    <Toast position="top-left" group="tl" />
+    <Toast position="top-right" group="tr" />
+    <Toast position="center" group="center" />
+    <Toast position="bottom-left" group="bl" />
+    <Toast position="bottom-right" group="br" style="word-break: break-all;" />
+    <Toast position="bottom-right" group="brt">
+      <template #message="slotProps">
+        <div style="width: 100%" class="grid grid-cols-12">
+          <div class="col-span-2">
+            <i class="pi pi-exclamation-triangle" style="font-size: 2.5rem"></i>
+          </div>
+          <div class="col-span-10">
+            <h3>{{slotProps.message.summary}}</h3>
+            <p>{{slotProps.message.detail}}</p>
+            <p>{{slotProps.message.detail2}}</p>
+          </div>  
+        </div>
+      </template>
+    </Toast>
     <headerComponent class=" flex-grow-0" />
     <router-view class="flex-grow container mx-auto pt-48 sm:pt-40"></router-view>
     <footerComponent class=" flex-grow-0" />
--- src/modules/transaction/views/ViewTransaction.vue
+++ src/modules/transaction/views/ViewTransaction.vue
@@ -3,15 +3,22 @@
     <p class="text-gray-500 mb-5 text-sm font-bold">
       Transaction Details
     </p>
-    <div class="filter shadow-xl border border-gray-50 p-5 mb-15">
-      <div class="flex items-center mb-4 border-b border-gray-100 relative">
-        <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'detail')?'cursor-pointer':'' }`" @click="currentPage = 'detail'">Overview<div v-if="currentPage == 'detail'" class="absolute w-full border-b border-yellow-500 transition-all duration-200" style="bottom: -1px;"></div></div>
-        <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'inner')?'cursor-pointer':'' }`" @click="currentPage = 'inner'">Inner Txns<div v-if="currentPage == 'inner'" class="absolute w-full border-b border-yellow-500 transition-all duration-200" style="bottom: -1px;"></div></div>
+    <div v-if="isFetching">
+      <div class="flex justify-center items-center border-gray-400 mt-15">
+        <div class="animate-spin rounded-full h-5 w-5 border-b-2 border-navy-primary mr-2"></div>
+        <span class="text-tsm">Fetching transaction</span>
       </div>
-      <transition name="slide">
-        <TxnDetailComponent v-if="currentPage == 'detail'" />
-        <InnerTxnComponent v-else />
-      </transition>
+    </div>
+    <div v-else>
+      <div class="filter shadow-xl border border-gray-50 p-5 mb-15" v-if="formattedTransaction.hash">
+        <div class="flex items-center mb-4 border-b border-gray-100 relative">
+          <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'detail')?'cursor-pointer':'' }`" @click="currentPage = 'detail'">Overview<div v-if="currentPage == 'detail'" class="absolute w-full border-b border-yellow-500 transition-all duration-200" style="bottom: -1px;"></div></div>
+          <div class="w-32 font-bold text-xs text-center p-2 relative" :class="`${ (currentPage != 'inner')?'cursor-pointer':'' }`" @click="currentPage = 'inner'" v-if="innerTransaction">Inner Txns<div v-if="currentPage == 'inner'" class="absolute w-full border-b border-yellow-500 transition-all duration-200" style="bottom: -1px;"></div></div>
+        </div>
+        <TxnDetailComponent v-if="currentPage == 'detail'" :txnDetail="formattedTransaction" />
+        <InnerTxnComponent v-else :innerTxn="innerTransaction" />
+      </div>
+      <div v-if="!formattedTransaction.status" class="p-3 bg-yellow-100 text-yellow-700">Transaction not found</div>
     </div>
   </div>
 </template>
@@ -21,6 +28,8 @@ import { computed, defineComponent, getCurrentInstance, inject, ref, watch } fro
 import TxnDetailComponent from '@/modules/transaction/components/TxnDetailComponent.vue';
 import InnerTxnComponent from '@/modules/transaction/components/InnerTxnComponent.vue';
 import { AppState } from '@/state/appState';
+import { Helper } from "@/util/typeHelper";
+import { TransactionUtils } from '@/models/util/transactionUtils';
 export default {
   name: 'ViewTransaction',
   components: {
@@ -30,17 +39,56 @@ export default {
   props: {
     hash: String
   },
-  setup(){
+  setup(props){
     const currentPage = ref('detail');
+    const isFetching = ref(true);
+    const formattedTransaction = ref({
+      hash: '',
+      status: '',
+      timestamp: '',
+      height: 0,
+      type: '',
+      fee: 0,
+      signature: '',
+      signer: '',
+      version: '',
+      isMissingSignature: false,
+      cosigners: 0
+    });
+    const innerTransaction = ref({});
+
+    (async() => {
+      let transaction = await TransactionUtils.getTransaction(props.hash);
+      if(transaction.status){
+        formattedTransaction.value = {
+          hash: props.hash,
+          status: transaction.txn.isConfirmed(),
+          timestamp: Helper.formatDeadline(transaction.txn.deadline.adjustedValue.compact()),
+          height: transaction.txn.transactionInfo.height.compact(),
+          type: TransactionUtils.getTransactionTypeName(transaction.txn.type),
+          fee: Helper.convertToExact(transaction.txn.maxFee.compact(), AppState.nativeToken.divisibility),
+          signature: transaction.txn.signature,
+          signer: transaction.txn.signer.address.address,
+          version: transaction.txn.version,
+          isMissingSignature: transaction.txn.hasMissingSignatures(),
+          cosigners: transaction.txn.cosignatures,
+          group: transaction.txnStatus.group,
+        }
+        innerTransaction.value = transaction.txn.innerTransactions;
+      }else{
+        formattedTransaction.value = transaction;
+      }
+
+      isFetching.value = false;
+    })();
+
     return {
       currentPage,
-      AppState
+      AppState,
+      formattedTransaction,
+      innerTransaction,
+      isFetching,
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
