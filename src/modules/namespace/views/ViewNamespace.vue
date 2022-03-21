2368713768eb2762ee3903dfff4e1533664524e8
Benjamin - workstation <benjamin@webtree.my>
2022-03-21 13:41:31 +0800

    Adjust margin for mobile view

--- src/modules/namespace/views/ViewNamespace.vue
+++ src/modules/namespace/views/ViewNamespace.vue
@@ -4,12 +4,12 @@
     <div class="p-3 bg-yellow-100 text-yellow-700">Namespace ID is not available in {{ networkName }}</div>
   </div>
   <div v-else>
-    <div class="text-gray-500 mb-5 font-bold mx-5 md:mx-0">
+    <div class="text-gray-500 mb-5 font-bold">
       <div class="text-blue-primary text-md font-bold uppercase">{{ namespaceInfo.name }}</div>
       <div class="text-txs text-gray-400 font-normal mt-1">Namespace</div>
     </div>
-    <div class="md:grid md:grid-cols-2 mx-5 md:mx-0">
-      <div class="filter shadow-xl border border-gray-50 p-5 mb-15 md:mr-2">
+    <div class="md:grid md:grid-cols-2">
+      <div class="filter shadow-xl border border-gray-50 p-5 mb-5 md:mb-15 md:mr-2">
         <div class="text-sm font-bold mb-5">Overview</div>
         <div class="txn-div">
           <div class="py-4 text-xs grid grid-cols-5 border-b border-gray-100">
@@ -34,7 +34,7 @@
           </div>
         </div>
       </div>
-      <div class="filter shadow-xl border border-gray-50 p-5 mb-15 sm:ml-2">
+      <div class="filter shadow-xl border border-gray-50 p-5 mb-5 md:mb-15 sm:ml-2">
         <div class="text-sm font-bold mb-5">More Info</div>
         <div class="txn-div">
           <div class="py-4 text-xs grid grid-cols-5 border-b border-gray-100">
@@ -56,7 +56,7 @@
         </div>
       </div>
     </div>
-    <div v-if="namespaceInfo.alias || namespaceInfo.levels" class="filter shadow-xl border border-gray-50 p-5 mb-15  mx-5 md:mx-0">
+    <div v-if="namespaceInfo.alias || namespaceInfo.levels" class="filter shadow-xl border border-gray-50 p-5 mb-10 md:mb-15">
       <div v-if="namespaceInfo.alias">
         <div class="text-sm font-bold mb-4">Alias</div>
         <div class="py-3 text-xs">
