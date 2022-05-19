d17709728b515c8d29bad08774490a199ba131f3
HAIENG <haieng.chong@proximax.io>
2022-05-19 23:26:03 +0800

    amount of asset transfer not shown

--- src/modules/transaction/components/transactionDetails/TransferDetailComponent.vue
+++ src/modules/transaction/components/transactionDetails/TransferDetailComponent.vue
@@ -17,7 +17,7 @@
         <img src="@/assets/img/icon-xpx.svg" class="ml-2 inline-block absolute" style="top: -1px; width:14px;" />
       </div>
     </div>
-    <div v-if="txnDetail.detail.assetAmount">
+    <!-- <div v-if="txnDetail.detail.assetAmount">
       <div>Amount</div>
       <div class="relative">
         <span class="font-bold">{{ Helper.toCurrencyFormat(assetAmount[0], nativeTokenDivisibility) }}</span>{{ assetAmount[1]>0?'.':'' }}<span class="text-xxs">{{ assetAmount[1] }}</span>
@@ -27,19 +27,19 @@
           <router-link :to="{ name: 'ViewAsset', params: { id: txnDetail.assetId }}" class="hover:text-blue-primary hover:underline">{{ txnDetail.assetId }}</router-link>
         </div>
       </div>
-    </div>
-    <div v-if="txnDetail.detail.amount">
+    </div> -->
+    <div v-if="txnDetail.detail.sda.length > 0">
       <div>SDA Amount</div>
       <div class="relative">
-        <div v-for="sda, item in txnDetail.detail.amount" :key="item">
+        <div v-for="sda, item in txnDetail.detail.sda" :key="item">
           <span class="font-bold">{{ sdaAmount[item][0] }}</span>{{ sdaAmount[item][1]>0?'.':'' }}<span class="text-xxs">{{ sdaAmount[item][1] }}</span>
           <div class="inline-block text-gray-400 text-txs hover:text-gray-700 duration-300 transition-all ml-1">
-            <router-link v-if="sda.name" :to="{ name: 'ViewNamespace', params: { namespaceParam: sda.namespaceID }}" class="hover:text-blue-primary hover:underline">{{ sda.name }}</router-link>
-            {{ sda.name?' / ':'' }}
-            <router-link :to="{ name: 'ViewAsset', params: { id: sda.id }}" class="hover:text-blue-primary hover:underline">{{ sda.id }}</router-link>
+            <router-link v-if="sda.id" :to="{ name: 'ViewAsset', params: { id: sda.id }}" class="hover:text-blue-primary hover:underline">{{ sda.sendWithAlias.name }}</router-link>
+            <!-- {{ sda.name?' / ':'' }} -->
+            <router-link v-else :to="{ name: 'ViewAsset', params: { id: sda.id }}" class="hover:text-blue-primary hover:underline">{{ sda.id }}</router-link>
           </div>
         </div>
-        <div v-if="txnDetail.detail.amount.length == 0">-</div>
+        <!-- <div v-if="txnDetail.detail.amount.length == 0">-</div> -->
       </div>
     </div>
     <div v-if="txnDetail.detail.message">
@@ -84,10 +84,15 @@ export default {
       }
     });
 
+       // console.log(props.txnDetail.detail.amount);
+        //console.log(props.txnDetail.detail.assetAmount);
+
     const sdaAmount = computed(() => {
       let formattedSDA = [];
-      if(props.txnDetail.detail.amount){
-        props.txnDetail.detail.amount.forEach(sda => {
+      if(props.txnDetail.detail.sda.length > 0){
+        props.txnDetail.detail.sda.forEach(sda => {
+                console.log(sda);
+
           formattedSDA.push(sda.amount.toString().split('.'));
         });
       }
