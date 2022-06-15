0095834a77ac70fcaf021d695983d65ba7baa6a4
HAIENG <haieng.chong@proximax.io>
2022-06-15 16:15:25 +0800

    metadata utils

--- src/util/metadataUtil.ts
+++ src/util/metadataUtil.ts
@@ -1,18 +1,15 @@
 import { AppState } from "@/state/appState";
-import { Convert, MetadataQueryParams, MetadataType, MosaicId, NamespaceId, PublicAccount } from "tsjs-xpx-chain-sdk";
+import { Convert, MetadataQueryParams, MetadataType, MosaicId, NamespaceId, PublicAccount, UInt64 } from "tsjs-xpx-chain-sdk";
 import isValidUTF8 from 'utf-8-validate';
+
 export interface MetadataObj {
     scopedMetadataKeyUtf8: string;
     scopedMetadataKeyHex: string;
-    name: string,
-    id: string;
     value: string;
 }
 
 export class MetadataUtils {
-
-
-    static removeDoubleZero(string: string){
+    static removeDoubleZero(string: string) {
         let isZero = string.endsWith('00')
         if (isZero) {
             string = string.substring(0, string.length - 2);
@@ -21,79 +18,82 @@ export class MetadataUtils {
         return string;
     }
 
-    static convertUtf8 (scopedMetadataKey: string):string{
-        scopedMetadataKey =  MetadataUtils.removeDoubleZero(scopedMetadataKey )
-        let bytes = Convert.hexToUint8(scopedMetadataKey );
-        if(isValidUTF8(bytes)){
-            scopedMetadataKey  = Convert.decodeHexToUtf8(scopedMetadataKey)
+    static convertUtf8(scopedMetadataKey: string): string {
+        scopedMetadataKey = MetadataUtils.removeDoubleZero(scopedMetadataKey)
+        let bytes = Convert.hexToUint8(scopedMetadataKey);
+        if (isValidUTF8(bytes)) {
+            scopedMetadataKey = Convert.decodeHexToUtf8(scopedMetadataKey)
         }
         return scopedMetadataKey
     }
     
     static async getAccountMetadata(publicKey: string): Promise<MetadataObj[]> {
-        let accountMetadata: MetadataObj;
-        let accountMetadataList: any = [];
-        let metadataQueryParams = new MetadataQueryParams();
-        metadataQueryParams.metadataType = MetadataType.ACCOUNT;
-        metadataQueryParams.targetKey = PublicAccount.createFromPublicKey(publicKey, AppState.networkType);
-        let fetchMetadata = await AppState.chainAPI.metadataAPI.searchMetadatas(metadataQueryParams);
-        fetchMetadata.metadataEntries.forEach(metadataEntry => {            
-            accountMetadata = {
-                scopedMetadataKeyUtf8: metadataEntry.scopedMetadataKey.toHex() == this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()) ? null : this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()),
-                scopedMetadataKeyHex: metadataEntry.scopedMetadataKey.toHex(),
-                name: "",
-                id: "-",
-                value: metadataEntry.value
-            }
-            accountMetadataList.push(accountMetadata);
-        })
-
-        return accountMetadataList;
-    }
-
-    static async getAssetMetadata(mosaic: any = []): Promise<MetadataObj[]> {
-        console.log(mosaic);
-        let assetMetadata: MetadataObj;
-        let assetMetadataList: any = [];
-        let metadataQueryParams = new MetadataQueryParams();
-        mosaic.forEach(async (mosaicId) => {
-            console.log(mosaicId.id);
-            metadataQueryParams.metadataType = MetadataType.MOSAIC;
-            metadataQueryParams.targetId = new MosaicId(mosaicId.id);
+        try{
+            let accountMetadata: MetadataObj;
+            let accountMetadataList: any = [];
+            let metadataQueryParams = new MetadataQueryParams();
+            metadataQueryParams.metadataType = MetadataType.ACCOUNT;
+            metadataQueryParams.targetKey = publicKey;
             let fetchMetadata = await AppState.chainAPI.metadataAPI.searchMetadatas(metadataQueryParams);
-            console.log(fetchMetadata);
+
             fetchMetadata.metadataEntries.forEach(metadataEntry => {
-                console.log(metadataEntry.targetId);
-                assetMetadata = {
+                accountMetadata = {
                     scopedMetadataKeyUtf8: metadataEntry.scopedMetadataKey.toHex() == this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()) ? null : this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()),
                     scopedMetadataKeyHex: metadataEntry.scopedMetadataKey.toHex(),
-                    name: "",
-                    id: mosaicId.id,
                     value: metadataEntry.value
                 }
-                assetMetadataList.push(assetMetadata);
+                accountMetadataList.push(accountMetadata);
             })
-        })
-        return assetMetadataList;
+            return accountMetadataList;
+        } catch(e) {
+            console.error(e);
+        }
     }
 
-    static async getNamespaceMetadata(namespaceId: string): Promise<MetadataObj[]> {
+    static async getAssetMetadata(mosaic: string): Promise<MetadataObj[]> {
+        try {
+            let assetMetadata: MetadataObj;
+            let assetMetadataList: any = [];
+            let metadataQueryParams = new MetadataQueryParams();
+                metadataQueryParams.metadataType = MetadataType.MOSAIC;
+            metadataQueryParams.targetId = new MosaicId(mosaic);
+                let fetchMetadata = await AppState.chainAPI.metadataAPI.searchMetadatas(metadataQueryParams);
+                fetchMetadata.metadataEntries.forEach(metadataEntry => {
+                    assetMetadata = {
+                        scopedMetadataKeyUtf8: metadataEntry.scopedMetadataKey.toHex() == this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()) ? null : this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()),
+                        scopedMetadataKeyHex: metadataEntry.scopedMetadataKey.toHex(),
+                        value: metadataEntry.value
+                    }
+                    assetMetadataList.push(assetMetadata);
+                })
+            return assetMetadataList;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    static async getNamespaceMetadata(namespaceName: string): Promise<MetadataObj[]> {
+        try {
         let namespaceMetadata: MetadataObj;
         let namespaceMetadataList: any = [];
         let metadataQueryParams = new MetadataQueryParams();
-        metadataQueryParams.metadataType = MetadataType.NAMESPACE;
-        metadataQueryParams.targetId = new NamespaceId(namespaceId);
-        let fetchMetadata = await AppState.chainAPI.metadataAPI.searchMetadatas(metadataQueryParams);
-        fetchMetadata.metadataEntries.forEach(metadataEntry => {
-            namespaceMetadata = {
-                scopedMetadataKeyUtf8: metadataEntry.scopedMetadataKey.toHex() == this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()) ? null : this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()),
-                scopedMetadataKeyHex: metadataEntry.scopedMetadataKey.toHex(),
-                name: "",
-                id: namespaceId,
-                value: metadataEntry.value
-            }
-            namespaceMetadataList.push(namespaceMetadata);
-        })
+            metadataQueryParams.metadataType = MetadataType.NAMESPACE;
+            metadataQueryParams.targetId = new NamespaceId(namespaceName);
+            let fetchMetadata = await AppState.chainAPI.metadataAPI.searchMetadatas(metadataQueryParams);
+
+            fetchMetadata.metadataEntries.forEach(metadataEntry => {
+                namespaceMetadata = {
+                    scopedMetadataKeyUtf8: metadataEntry.scopedMetadataKey.toHex() == this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()) ? null : this.convertUtf8(metadataEntry.scopedMetadataKey.toHex()),
+                    scopedMetadataKeyHex: metadataEntry.scopedMetadataKey.toHex(),
+                    value: metadataEntry.value
+                }
+                namespaceMetadataList.push(namespaceMetadata);
+            })
+        
         return namespaceMetadataList;
+        } catch (e) {
+            console.error(e);
+        }
     }
+        
 }
