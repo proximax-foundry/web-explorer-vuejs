9f122f64cb9fe6410921c5cd746afae753931cc0
Benjamin Lai <laics1984@gmail.com>
2022-06-16 10:50:07 +0800

    Fixed account tabs

--- src/modules/account/views/ViewAccount.vue
+++ src/modules/account/views/ViewAccount.vue
@@ -7,12 +7,12 @@
         <AccountComponent :address="strAddress" :publicKey="strPublicKey" :linkAccount="delegatePublicKey" :namespace="matchedNamespace" :multisig="cosignatoriesLength" class="mb-10" />
       </div>
       <div class="flex text-xs font-semibold border-b-2 menu_title_div">
-        <div class="w-18 text-center cursor-pointer pb-3" :class="`${ (currentComponent=='asset')?'border-yellow-500 border-b-2':'' }`" @click="setCurrentComponent('asset')">Assets</div>
-        <div class="w-18 text-center cursor-pointer" :class="`${ (currentComponent=='namespace')?'border-yellow-500 border-b-2':'' }`" @click="setCurrentComponent('namespace')" v-if="accountNamespaces.length > 0">Namespaces</div>
-        <div class="w-18 text-center cursor-pointer" :class="`${ (currentComponent=='metadata')?'border-yellow-500 border-b-2':'' }`" @click="setCurrentComponent('metadata')" v-if="accountMetadata.length > 0">Metadata</div>
-        <div class="w-18 text-center cursor-pointer" :class="`${ (currentComponent=='multisig')?'border-yellow-500 border-b-2':'' }`" @click="setCurrentComponent('multisig')" v-if="multisigLength > 0 || cosignatoriesLength > 0">Multisig</div>
-        <div class="w-18 text-center cursor-pointer" :class="`${ (currentComponent=='scheme')?'border-yellow-500 border-b-2':'' }`" @click="setCurrentComponent('scheme')" v-if="cosignatoriesLength > 0">Scheme</div>
-        <div class="w-18 text-center cursor-pointer" :class="`${ (currentComponent=='txn')?'border-yellow-500 border-b-2':'' }`" @click="setCurrentComponent('txn')">Transactions</div>
+        <div class="w-18 text-center pb-3" :class="`${ (currentComponent=='asset')?'border-yellow-500 border-b-2':'cursor-pointer' }`" @click="setCurrentComponent('asset')">Assets</div>
+        <div class="w-18 text-center" :class="`${ (currentComponent=='namespace')?'border-yellow-500 border-b-2':'cursor-pointer' }`" @click="setCurrentComponent('namespace')" v-if="accountNamespaces.length > 0">Namespaces</div>
+        <div class="w-18 text-center" :class="`${ (currentComponent=='metadata')?'border-yellow-500 border-b-2':'cursor-pointer' }`" @click="setCurrentComponent('metadata')" v-if="accountMetadata.length > 0">Metadata</div>
+        <div class="w-18 text-center" :class="`${ (currentComponent=='multisig')?'border-yellow-500 border-b-2':'cursor-pointer' }`" @click="setCurrentComponent('multisig')" v-if="multisigLength > 0 || cosignatoriesLength > 0">Multisig</div>
+        <div class="w-18 text-center" :class="`${ (currentComponent=='scheme')?'border-yellow-500 border-b-2':'cursor-pointer' }`" @click="setCurrentComponent('scheme')" v-if="cosignatoriesLength > 0">Scheme</div>
+        <div class="w-18 text-center" :class="`${ (currentComponent=='txn')?'border-yellow-500 border-b-2':'cursor-pointer' }`" @click="setCurrentComponent('txn')">Transactions</div>
       </div>
       <div class="mb-20" v-if="!isFetching">
         <AssetComponent :accountAssets="accountAssets" v-if="currentComponent=='asset'" />
