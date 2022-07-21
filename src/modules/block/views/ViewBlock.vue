c07671549d660d462b90df6af5ff0983d13aec03
HAIENG <haieng.chong@proximax.io>
2022-07-21 14:48:35 +0800

    missed out pagination and ui fix in blocklist trx

--- src/modules/block/views/ViewBlock.vue
+++ src/modules/block/views/ViewBlock.vue
@@ -58,7 +58,25 @@
         Transactions
         </p>
         <div class="filter shadow-xl border border-gray-50 p-5 mb-15">
-          <TransactionComponent :blockHeight="blockInfo.height" />
+          <MixedTxnDataTable :transactions="transactions" :pages="pages" />
+           <div class="sm:flex sm:justify-between my-5 mb-15" v-if="totalPages > 1">
+      <div class="text-xs text-gray-700 mb-3 sm:mb-0 text-center sm:text-left">Show
+        <select v-model="pages" class="border border-gray-300 rounded-md p-1" @change="changeRows">
+          <option value=20>20</option>
+          <option value=30>30</option>
+          <option value=40>40</option>
+          <option value=50>50</option>
+        </select>
+        Records
+      </div>
+      <div class="sm:flex sm:items-center text-center sm:text-right">
+        <div v-if="enableFirstPage" @click="naviFirst" class="bg-blue-100 inline-block border border-blue-100 rounded-sm px-2 py-1 text-blue-700 text-xs mx-1 cursor-pointer hover:bg-blue-200 duration-300 transition-all">First</div><div v-else class="bg-gray-50 inline-block border border-gray-50 rounded-sm px-2 py-1 text-gray-700 text-xs mx-1">First</div>
+        <div v-if="enablePreviousPage" @click="naviPrevious" class="bg-blue-100 inline-block border border-blue-100 rounded-sm px-2 py-1 text-blue-700 text-xs mx-1 cursor-pointer hover:bg-blue-200 duration-300 transition-all">Previous</div><div v-else class="bg-gray-50 inline-block border border-gray-50 rounded-sm px-2 py-1 text-gray-700 text-xs mx-1">Previous</div>
+        <div class="bg-gray-50 inline-block border border-gray-50 rounded-sm px-2 py-1 text-gray-700 text-xs">Page {{ currentPage }} of {{ totalPages }}</div>
+        <div v-if="enableNextPage" @click="naviNext" class="bg-blue-100 inline-block border border-blue-100 rounded-sm px-2 py-1 text-blue-700 text-xs mx-1 cursor-pointer hover:bg-blue-200 duration-300 transition-all">Next</div><div v-else class="bg-gray-50 inline-block border border-gray-50 rounded-sm px-2 py-1 text-gray-700 text-xs mx-1">Next</div>
+        <div v-if="enableLastPage" @click="naviLast" class="bg-blue-100 inline-block border border-blue-100 rounded-sm px-2 py-1 text-blue-700 text-xs ml-1 cursor-pointer hover:bg-blue-200 duration-300 transition-all">Last</div><div v-else class="bg-gray-50 inline-block border border-gray-50 rounded-sm px-2 py-1 text-gray-700 text-xs mx-1">Last</div>
+      </div>
+    </div>
         </div>
       </div>
     </div>
@@ -70,14 +88,16 @@ import { computed, getCurrentInstance, ref, watch } from "vue";
 import { BlockUtils } from "@/util/blockUtil"
 import { AppState } from '@/state/appState';
 import { networkState } from '@/state/networkState';
-import TransactionComponent from '@/modules/block/components/TransactionComponent.vue';
+import MixedTxnDataTable from '@/modules/transaction/components/txnDataTables/MixedTxnDataTable';
+// import TransactionComponent from '@/modules/block/components/TransactionComponent.vue';
 import { Helper } from "@/util/typeHelper";
 import { TransactionUtils } from '@/models/util/transactionUtils';
+import { TransactionGroupType,TransactionQueryParams } from 'tsjs-xpx-chain-sdk';
 import { copyToClipboard } from '@/util/functions';
 import { useToast } from "primevue/usetoast";
 
 export default {
-  components: { TransactionComponent },
+  components: { MixedTxnDataTable },
   name: 'ViewBlock',
   props: {
     blockHeight: Number
@@ -87,6 +107,7 @@ export default {
     const internalInstance = getCurrentInstance();
     const emitter = internalInstance.appContext.config.globalProperties.emitter;
     const blockInfo = ref([]);
+    const transactions = ref([]);
     const isShowInvalid = ref(null);
     const nativeTokenNamespace = AppState.nativeToken.label;
     const copy = (id) =>{ 
@@ -95,6 +116,7 @@ export default {
       copyToClipboard(stringToCopy);
       toast.add({severity:'info', detail: copySubject + ' copied', group: 'br', life: 3000});
     };
+
     const loadBlock = async() =>{
       const block = await BlockUtils.getBlockByHeight(p.blockHeight);
         if(!AppState.isReady){
@@ -108,8 +130,57 @@ export default {
         }else{
           isShowInvalid.value = true;
         }
+        getTransactions();
+
     };  
     loadBlock();
+
+    const pages = ref(20);
+    const currentPage = ref(1)
+    const totalPages = ref(0);
+
+    
+    const enableFirstPage = computed(() => {
+      return currentPage.value > 1;
+    });
+
+    const enablePreviousPage = computed(() => {
+      return currentPage.value > 1;
+    });
+
+    const enableNextPage = computed(() => {
+      return (totalPages.value - currentPage.value) > 0;
+    });
+
+    const enableLastPage = computed(() => {
+      return currentPage.value < totalPages.value;
+    });
+
+    const naviFirst = () => {
+      currentPage.value = 1;
+      getTransactions();
+    }
+
+    const naviPrevious = () => {
+      --currentPage.value;
+      getTransactions();
+    }
+
+    const naviNext = () => {
+      ++currentPage.value;
+      getTransactions();
+    }
+
+    const naviLast = () => {
+      currentPage.value = totalPages.value;
+      getTransactions();
+    }
+
+    const changeRows = () => {
+      currentPage.value = 1;
+      getTransactions();
+    }
+
     const networkName = computed(() => {
       return networkState.chainNetworkName;
     });
@@ -120,6 +191,21 @@ export default {
       }
     });
 
+    const getTransactions = async() =>{
+      if(p.blockHeight != null){
+        let txnQueryParams = new TransactionQueryParams();
+        txnQueryParams.height = p.blockHeight;
+        txnQueryParams.pageSize = pages.value;
+        txnQueryParams.pageNumber = currentPage.value;
+        let txns = await TransactionUtils.searchTxns(TransactionGroupType.CONFIRMED,txnQueryParams);
+        if(txns.transactions.length > 0){
+          let transactionSearchResult = await TransactionUtils.formatConfirmedMixedTxns(txns.transactions);
+          transactions.value = transactionSearchResult;
+          totalPages.value = transactionSearchResult.pagination.totalPages;
+        }
+      }
+    }
+    getTransactions();
     const currencyDivisibility = computed(() => {
       return AppState.nativeToken.divisibility;
     })
@@ -132,7 +218,20 @@ export default {
       Helper,
       currencyDivisibility,
       TransactionUtils,
-      copy
+      copy,
+      transactions,
+      pages,
+      currentPage,
+      totalPages,
+      enableFirstPage,
+      enablePreviousPage,
+      enableNextPage,
+      enableLastPage,
+      naviFirst,
+      naviPrevious,
+      naviNext,
+      naviLast,
+      changeRows
     }
   }
 }
