62862ef75776db7fd949294daed186579914c4db
Ghost-Slime <kongsehlen@gmail.com>
2023-12-08 14:50:45 +0800

    remove metadata selection, add more details

--- src/modules/metadata/components/MetadataComponent.vue
+++ src/modules/metadata/components/MetadataComponent.vue
@@ -44,8 +44,29 @@
         </Column>
         <Column style="width: 10px" v-if="wideScreen"> </Column>
         <Column
-          field="Block"
-          header="Block"
+          field="Scoped Metadata Key"
+          header="Scoped Metadata Key"
+          style="width: 250px; font-size: 12px;"
+          v-if="wideScreen"
+        >
+          <template #body="{ data }">
+            <div class="text-xs" v-if="data.scopedMetadataKeyHex">
+              {{ data.scopedMetadataKeyHex }}
+              <div class="inline-block ml-2 font-semibold text-gray-400">
+                hex
+              </div>
+            </div>
+            <div class="mt-1 text-xs" v-if="data.scopedMetadataKeyUtf8">
+              {{ data.scopedMetadataKeyUtf8 }}
+              <div class="inline-block ml-2 font-semibold text-gray-400">
+                utf-8
+              </div>
+            </div>
+          </template>
+        </Column>
+        <Column
+          field="Creation Block"
+          header="Creation Block"
           style="width: 130px; font-size: 12px;"
           v-if="wideScreen"
         >
@@ -54,9 +75,9 @@
           </template>
         </Column>
         <Column
-          field="Timestamp"
-          header="Timestamp"
-          style="width: 170px; font-size: 12px;"
+          field="Creation Timestamp"
+          header="Creation Timestamp"
+          style="width: 200px; font-size: 12px;"
           v-if="wideScreen"
         >
           <template #body="{ data }">
@@ -64,8 +85,48 @@
           </template>
         </Column>
         <Column
-          field="Value"
-          header="Value"
+          field="Account Public Key"
+          header="Account Public Key"
+          style="width: 700px; font-size: 12px;"
+          v-if="wideScreen"
+        >
+          <template #body="{ data }">
+            <span class="text-xs">{{ data.accPublicKey }}</span>
+          </template>
+        </Column>
+        <Column
+          field="Asset ID"
+          header="Asset ID"
+          style="width: 200px; font-size: 12px;"
+          v-if="wideScreen && isAsset"
+        >
+          <template #body="{ data }">
+            <span class="text-xs">{{ data.assetId }}</span>
+          </template>
+        </Column>
+        <Column
+          field="Namespace"
+          header="Namespace"
+          style="width: 200px; font-size: 12px;"
+          v-if="wideScreen && isNamespace"
+        >
+          <template #body="{ data }">
+            <span class="text-xs">{{ data.namespace }}</span>
+          </template>
+        </Column>
+        <Column
+          field="Metadata Type"
+          header="Metadata Type"
+          style="width: 100px; font-size: 12px;"
+          v-if="wideScreen"
+        >
+          <template #body="{ data }">
+            <span class="text-xs">{{ data.type }}</span>
+          </template>
+        </Column>
+        <Column
+          field="Creation Value"
+          header="Creation Value"
           style="width: 100px; font-size: 12px;"
           v-if="wideScreen"
         >
@@ -89,6 +150,8 @@ defineProps({
     type: Array<MetadataObj>,
     required: true,
   },
+  isAsset: Boolean,
+  isNamespace: Boolean,
 });
 const wideScreen = ref(false);
 const screenResizeHandler = () => {
--- src/modules/metadata/views/ViewMetadata.vue
+++ src/modules/metadata/views/ViewMetadata.vue
@@ -4,50 +4,11 @@
       Metadata is not available in {{ networkName }}
     </div>
   </div>
-  <div v-else-if="selectedMetadataDetail">
-      <div class="text-gray-500 mb-1 text-sm font-bold">
-        {{ totalMetadataList[selectedMetadata] }}
-      </div>
-      <div
-      @click="toggleSelection = !toggleSelection"
-      class="text-txs text-gray-400 font-normal mt-1"
-    >
-    <div class="flex">
-      <div class="text-tsm font-bold">Select Metadata Record</div>
-      <img
-        src="@/assets/img/icon-caret-down-black.svg"
-        class="ml-2 transition-all duration-200"
-        :class="`${toggleSelection ? 'rotate-180 transform' : ''}`"
-      />
+  <div v-else>
+    <div class="text-gray-500 mb-1 text-sm font-bold">
+        Metadata History
     </div>
-    </div>
-    <div class="relative">
-      <div
-        v-if="toggleSelection"
-        class="absolute border border-t-0 z-50 bg-white max-h-52 overflow-auto px-1 filter drop-shadow-xl pb-2"
-      >
-        <div
-          v-if="totalMetadataList.length > 0"
-          class="pl-2 pt-4 text-xxs text-gray-400"
-        >
-        </div>
-        <div v-else class="text-xxs pt-2 pl-2 pb-2">The list is empty.</div>
-        <div
-          v-for="(items, index) in totalMetadataList"
-          :key="index"
-          class="px-2 py-3 flex cursor-pointer items-center hover:bg-gray-50 transition-all duration-300"
-          @click="selectedMetadata = index, toggleSelection = !toggleSelection"
-          :class="`${
-            index != totalMetadataList.length - 1 ? 'border-b border-gray-200' : ''
-          }`"
-        >
-          <div>
-            <div class="text-xs font-semibold">{{ items }}</div>
-          </div>
-        </div>
-        </div>
-    </div>
-    <div v-if="!isShowInvalid && !selectedMetadataDetail">
+    <div v-if="!isShowInvalid && !displayMetadataTable">
       <div class="flex justify-center items-center border-gray-400 mt-10 mb-20">
         <div
           class="animate-spin rounded-full h-5 w-5 border-b-2 border-navy-primary mr-2"
@@ -55,54 +16,25 @@
         <span class="text-tsm">Fetching Metadata Details</span>
       </div>
     </div>
-    <div v-else class="mt-3">
-      <div class="md:grid md:grid-cols-2">
-        <div class="filter shadow-xl border border-gray-50 p-5 mb-15 md:mr-2">
-          <div class="text-xs font-bold mb-5 ml-2">Overview</div>
-          <div class="txn-div">
-            <div class="py-4 text-xs grid grid-cols-5 border-b border-gray-100">
-              <div class="font-bold col-span-2">Scoped Metadata Key</div>
-              <div class="font-bold uppercase">{{ selectedMetadataDetail.scopedMetadataKeyHex }}</div>
-            </div>
-            <div class="py-4 text-xs grid grid-cols-5 border-b border-gray-100">
-              <div class="font-bold col-span-2">Value</div>
-              <div class="font-bold">{{ selectedMetadataDetail.value }}</div>
-            </div>
-          </div>
-        </div>
-        <div class="filter shadow-xl border border-gray-50 p-5 mb-15 sm:ml-2">
-          <div class="text-xs font-bold mb-5 ml-2">More Info</div>
-          <div class="txn-div">
-            <div class="py-4 text-xs grid grid-cols-5 border-b border-gray-100">
-              <div class="font-bold col-span-2">Block</div>
-              <div class="col-span-3">
-                {{ selectedMetadataDetail.block }}
-              </div>
-            </div>
-            <div class="py-4 text-xs grid grid-cols-5 border-b border-gray-100">
-              <div class="font-bold col-span-2">Timestamp</div>
-              <div class="col-span-3">{{ selectedMetadataDetail.timestamp }}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="filter shadow-xl border border-gray-50 p-5 mb-15">
+    <div v-else class="filter shadow-xl border border-gray-50 p-5 mb-15">
       <MetadataComponent
         :metadata="displayMetadataTable"
+        :isAsset="checkAssetId"
+        :isNamespace="checkNamespace"
       />
     </div>
   </div>
 </template>
   
   <script setup lang="ts">
-  import { computed, getCurrentInstance, ref, watch } from "vue";
+  import { computed, getCurrentInstance, ref } from "vue";
   import { Helper } from "@/util/typeHelper";
   import { networkState } from "@/state/networkState";
-  import { AccountMetadataTransaction, Convert, Deadline, MetadataEntry, MetadataType, MosaicMetadataTransaction, NamespaceMetadataTransaction, Transaction, TransactionGroupType, TransactionQueryParams, TransactionType } from "tsjs-xpx-chain-sdk";
+  import { AccountMetadataTransaction, Convert, Deadline, MetadataEntry, MetadataType, MosaicMetadataTransaction, NamespaceMetadataTransaction, TransactionGroupType, TransactionType } from "tsjs-xpx-chain-sdk";
   import { TransactionUtils } from "@/util/transactionUtils";
   import { AppState } from "@/state/appState";
   import MetadataComponent from "@/modules/metadata/components/MetadataComponent.vue";
+import { MetadataUtils } from "@/util/metadataUtil";
   
   const props = defineProps({
     compositeHash: {
@@ -115,23 +47,23 @@
     block: number;
     timestamp: string;
     scopedMetadataKeyHex: string;
+    scopedMetadataKeyUtf8: string;
+    accPublicKey: string;
+    assetId: string | null;
+    namespace: string | null;
+    type: string;
     value: string;
   }
   const internalInstance = getCurrentInstance();
   const emitter = internalInstance?.appContext.config.globalProperties.emitter;
   const isShowInvalid = ref(true);
-  const toggleSelection = ref(false);
   const metadataHistory = ref<any[]>([])
   const metadata = ref<string | null>(null)
   const allMetadata = ref<string | null>(null)
-  const selectedMetadataDetail = ref<MetadataObj | null>(null)
-  const totalMetadataList = ref<any[]>([])
-  const selectedMetadata = ref<number>(0)
   const displayMetadataTable = ref<MetadataObj[]>([])
-
-  const handleScroll = () => {
-    toggleSelection.value = false
-  };
+  const metadataHistoryType = ref<string>("")
+  const checkAssetId = ref<boolean>(false)
+  const checkNamespace = ref<boolean>(false)
   
   const networkName = computed(() => {
     return networkState.chainNetworkName;
@@ -159,12 +91,15 @@
     switch(metadata.metadataType){
         case MetadataType.ACCOUNT:
           txnQueryParams.type = [TransactionType.ACCOUNT_METADATA_V2]
+          metadataHistoryType.value = "Account"
           break;
         case MetadataType.MOSAIC:
           txnQueryParams.type = [TransactionType.MOSAIC_METADATA_V2]
+          metadataHistoryType.value = "Mosaic"
           break;
         case MetadataType.NAMESPACE:
           txnQueryParams.type = [TransactionType.NAMESPACE_METADATA_V2]
+          metadataHistoryType.value = "Namespace"
           break;
         default:
           txnQueryParams.type = undefined
@@ -200,28 +135,11 @@
     let metadataResult = await checkMetadataDetail()
     if(metadataResult){
       let getMetadataTxns = await fetchMetadataTxns(metadataResult)
-      for(let i = 0; i < getMetadataTxns.length; i++){
-        totalMetadataList.value.push(`Metadata ${i + 1}`)
-      }
       isShowInvalid.value = false
       return getMetadataTxns
     }
     else{
       return [];
-    }
-  }
-
-  const loadSelectedMetadataHistory = async () => {
-    let selectedMetadataTxn = metadataHistory.value[selectedMetadata.value]
-    let blockHeight = selectedMetadataTxn.transactionInfo!.height.compact()
-    const blockInfo = await AppState.chainAPI!.blockAPI.getBlockByHeight(
-      blockHeight
-    );
-    selectedMetadataDetail.value = {
-      block: blockInfo.height.compact(),
-      timestamp: Helper.convertDisplayDateTimeFormat24(new Date(blockInfo.timestamp.compact() + Deadline.timestampNemesisBlock * 1000).toISOString()),
-      scopedMetadataKeyHex: selectedMetadataTxn.scopedMetadataKey.toHex(),
-      value: await selectedValueChangeMetadata()
     }
   }
 
@@ -236,36 +154,37 @@
       block: blockInfo.height.compact(),
       timestamp: Helper.convertDisplayDateTimeFormat24(new Date(blockInfo.timestamp.compact() + Deadline.timestampNemesisBlock * 1000).toISOString()),
       scopedMetadataKeyHex: metadataTxn.scopedMetadataKey.toHex(),
+      scopedMetadataKeyUtf8: MetadataUtils.convertUtf8(metadataTxn.scopedMetadataKey.toHex()),
+      accPublicKey: metadataTxn.targetPublicKey.publicKey,
+      assetId: getMetadataAssetId(metadataTxn),
+      namespace: await getMetadataNamespace(metadataTxn),
+      type: metadataHistoryType.value,
       value: await allValueChangeMetadata(metadataTxn)
     })
     }
   }
 
-  const selectedValueChangeMetadata = async () => {
-    let txns = metadataHistory.value
-    for(let i = 0; i<= selectedMetadata.value; i++){
-      if (!metadata.value) {
-        const newValue = TransactionUtils.applyValueChange(
-          "",
-          Convert.uint8ArrayToHex(
-            txns[i].valueDifferences
-          ),
-          txns[i].valueSizeDelta
-        );
-        metadata.value = newValue
-      }
-      else{
-        const newValue = TransactionUtils.applyValueChange(
-          metadata.value,
-          Convert.uint8ArrayToHex(
-            txns[i].valueDifferences
-          ),
-          txns[i].valueSizeDelta
-        );
-        metadata.value = newValue
-      }
+  const getMetadataAssetId = (metadataTxn: any) => {
+    if(metadataTxn.targetMosaicId){
+      checkAssetId.value = true
+      return metadataTxn.targetMosaicId.toHex()
     }
-    return metadata.value? metadata.value : ""
+    else{
+      return null
+    }
+  }
+
+  const getMetadataNamespace = async (metadataTxn: any) => {
+    if(metadataTxn.targetNamespaceId){
+      checkNamespace.value = true
+      const nsName = await AppState.chainAPI!.namespaceAPI.getNamespacesName([
+        metadataTxn.targetNamespaceId,
+        ]);
+        return nsName[0].name;
+    }
+    else{
+      return null
+    }
   }
 
   const allValueChangeMetadata = async (metadataTxn: any) => {
@@ -294,31 +213,18 @@
 
   const init = async () =>{
     metadataHistory.value = await loadMetadataTxns()
-    loadSelectedMetadataHistory()
     loadAllMetadataHistory()
   }
 
   init()
-  
-  watch(selectedMetadata, (newValue,oldValue)=>{
-    if(newValue !== oldValue){
-      metadata.value = null
-      loadSelectedMetadataHistory()
-    }
-  })
-
-  window.addEventListener('scroll', handleScroll);
 
   emitter.on("CHANGE_NETWORK", (payload: boolean) => {
     if (payload) {
       isShowInvalid.value = true;
-      selectedMetadata.value = 0
       metadata.value = null
       allMetadata.value = null
-      totalMetadataList.value = []
       metadataHistory.value = []
       displayMetadataTable.value = []
-      selectedMetadataDetail.value = null
       init()
     }
   });
