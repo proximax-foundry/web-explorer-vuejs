2ffff71f83e34528dccf88b05365dc840763aae3
Chun Lam <31641877+sleepyOwl14@users.noreply.github.com>
2024-04-09 00:45:37 +0800

    fix asset flags handle (#268)
    * add additional flags
    
    * fix missing flags handle
    
    * remove txn builder

--- src/models/transactions/unconfirmed/asset.ts
+++ src/models/transactions/unconfirmed/asset.ts
@@ -13,8 +13,11 @@ export class UnconfirmedAssetTransaction extends UnconfirmedTransaction {
   supplyDelta?: number | null = null;
   supplyDirection?: number | null = null;
   divisibility?: number | null = null;
-  transferable?: boolean | null = null;
   supplyMutable?: boolean | null = null;
+  transferable?: boolean | null = null;
+  restrictable?: boolean | null = null;
+  supplyForceImmutable?: boolean | null = null;
+  disableLocking?: boolean | null = null;
   duration?: number | null = null;
 
   constructor(txnHash: string) {
